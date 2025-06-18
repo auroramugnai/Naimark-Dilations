d631ffbdfbf2cf1ee184c32c56037f4b53911102
Raffaele D'avino <rdavino@icfo.net>
2025-06-18 17:00:52 +0200

    Num_attempt changed

--- non-extremality_test.py
+++ non-extremality_test.py
@@ -4,7 +4,7 @@ import qutip as qt
 from picos.modeling.problem import Problem
 from tqdm import tqdm
 
-num_attempt = 20 # Hello hihi :3
+num_attempt = 2
 
 # Define identity matrices
 I2 = qt.qeye([2])
