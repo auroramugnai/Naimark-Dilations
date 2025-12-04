1d6af78ac8fb1319967730441cbeac1c605b7340
aurora <a.mugnai2@studenti.unipi.it>
2025-12-04 11:31:49 +0100

    Type annotations added.

--- ppt.py
+++ ppt.py
@@ -2,8 +2,13 @@
 This script builds an entangled basis for the bipartite system S+A, constructs the corresponding
 entangled density matrix, and runs a seesaw optimization algorithm to test whether there exists
 a separable rho_SA that reproduces both the same reduced state on S and the same measurement on S.
-"""
 
+Note on types: 
+    In the main code, all matrices are kept as numpy.ndarray, and all functions take numpy arrays as input.
+    Within the functions, inputs are automatically converted to qutip.Qobj when quantum operations (such as partial trace, 
+    tensor, or dagger) are needed,  and converted back to numpy.ndarray afterward -- 
+    except when PICOS objects are required for SDP optimization.
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 import picos as pic
@@ -11,13 +16,21 @@ from picos.modeling.problem import Problem
 import qutip as qt
 from qutip import Qobj
 from tqdm import tqdm
+from typing import List, Tuple, Union, Any
 
 # ----------------------------------------------------------------------
 # Configure numerical precision for PICOS solvers
 # ----------------------------------------------------------------------
-def setNumericalPrecisionForSolver(problem):
+def setNumericalPrecisionForSolver(problem: Problem) -> None:
     """
-    Sets high numerical precision parameters for PICOS solvers.
+    Set high numerical-precision options for a PICOS Problem object.
+    Parameters
+    ----------
+        problem : Problem
+            A PICOS Problem instance whose `options` dict will be modified in-place.
+    Returns
+    -------
+        None
     """
     problem.options["rel_ipm_opt_tol"] = 1e-14
     problem.options["rel_prim_fsb_tol"] = 1e-14
@@ -26,18 +39,20 @@ def setNumericalPrecisionForSolver(problem):
     
 ################################################################################################
 ################################################################################################
-def buildEntangledBasis(theta, verbosity=0):
-    """Build the entangled basis states Ψ_i for SA as described in the paper "Quantifying...".
-    Parameters:
+def buildEntangledBasis(theta: float, verbosity: int = 0) -> List[Qobj]:
+    """Build an entangled basis states for the bipartite system S+A.
+    Parameters
     ----------
         theta : float
-            The angle parameter in the basis states.
-        verbosity : int
-            Level of verbosity for printing the basis states.
-    Returns:
+            The complex phase parameter appearing in the linear combination that defines
+            the entangled basis states.
+        verbosity : int, optional
+            If 1, the function prints information.
+    Returns
     -------
-        basis : list of Qobj
-            The list of entangled basis states Ψ_i.
+        basis : List[Qobj]
+            A list containing 4 `qutip.Qobj` kets (each of shape 4x1) representing
+            the entangled basis states.
     """
     eta=[1/np.sqrt(3),-1/np.sqrt(3),-1/np.sqrt(3),1/np.sqrt(3)]
     phi=[np.pi/4,7*np.pi/4,3*np.pi/4,5*np.pi/4]
@@ -67,19 +82,19 @@ def buildEntangledBasis(theta, verbosity=0):
 
 ################################################################################################
 ################################################################################################
-def check_SA_entanglement(rhoSA):
+def check_SA_entanglement(rhoSA: Qobj) -> None:
     """Check if 'rhoSA' is entangled by computing the Von Neumann entropies of the subsystems.
-    
     Parameters:
     ----------
     rhoSA : Qobj
-        The density matrix of the combined system S+A.
-
+        The density matrix (4x4) of the combined system S+A.
     Raises:
     ------
     AssertionError
         If the entropies of S and A are not equal or if they are zero.
     """
+    rhoSA = qt.Qobj(rhoSA) if not isinstance(rhoSA, qt.Qobj) else rhoSA
+
     # Partial traces
     rhoS = rhoSA.ptrace(0)
     rhoA = rhoSA.ptrace(1)
@@ -94,21 +109,23 @@ def check_SA_entanglement(rhoSA):
 
 ################################################################################################
 ################################################################################################
-def find_entangled_rhoSA(basis):
+def find_entangled_rhoSA(basis: List[Qobj]) -> Tuple[Qobj, np.ndarray]:   
     """Look for a probability distribution 'probabilities' that gives an entangled 'rhoSA'. 
        To do this, randomly generate probability distributions until you find 
        one that gives a Negative Partial Transpose (NPT) state.
     Parameters:
     ----------
-        basis : list of Qobj
+        basis : List[Qobj]
             The list of entangled basis states Ψ_i.
     Returns:
     -------
         rhoSA : Qobj
             The entangled density matrix of the combined system S+A.
         probabilities : np.ndarray
-            The probability distribution used to construct rhoSA.  
+            The probability distribution (length 4) used to construct rhoSA.  
     """
+    basis = [qt.Qobj(psi) if not isinstance(psi, qt.Qobj) else psi for psi in basis]
+
     while True:
         #### Generate a probability distribution ####
         probabilities = np.random.rand(4)
@@ -130,20 +147,23 @@ def find_entangled_rhoSA(basis):
 
 ################################################################################################
 ################################################################################################
-def find_sep_states_max_p_guess(rhoS, PiSA_list, verbosity=1):
+def find_sep_states_max_p_guess(rhoS: np.ndarray, 
+                                PiSA_list: List[np.ndarray], 
+                                verbosity: int = 1          ) -> Tuple[float, List[np.ndarray]]:
     """Find the set of states {rhoSA_i} that maximizes the guessing probability
        given a fixed reduced state rhoS and measurement PiSA.
     Parameters:
     ----------
         rhoS : np.ndarray
-            The reduced density matrix of system S.
-        PiSA : list of Qobj
-            The measurement operators on the combined system S+A.
+            The reduced density matrix (2x2) of system S.
+        PiSA : List[np.ndarray]
+            A list of `dim` measurement operators on S+A represented as 4x4 numpy arrays.
+        verbosity : int, optional
     Returns:
     -------
         p_guess : float
-            The optimal guessing probability.
-        rhoSA_list : list of np.ndarray
+            The optimal (maximized) guessing probability.
+        rhoSA_list : List[np.ndarray]
             The list of optimal states rhoSA_i.
     """
     dim = 4 # Dimension of the total system S+A
@@ -182,21 +202,26 @@ def find_sep_states_max_p_guess(rhoS, PiSA_list, verbosity=1):
 
 ################################################################################################
 ################################################################################################
-def find_meas_max_p_guess(M_S, rhoSA_list, verbosity=1):
+def find_meas_max_p_guess(M_S: List[np.ndarray],
+                          rhoSA_list: List[np.ndarray],
+                          verbosity: int = 1                ) -> Tuple[float, List[np.ndarray]]:
     """Find the set of measurement operators {PiSA_i} that maximizes the guessing probability
        given a fixed set of states rhoSA_i.
     Parameters:
     ----------
-        M_S : list of np.ndarray
-            The reduced measurement operators on system S.
-        rhoSA_list : list of np.ndarray
-            The list of states rhoSA_i.
+        M_S : List[ np.ndarray]
+            A list of `dim` reduced measurement operators on system S. Elements can be
+            provided as `qutip.Qobj` or as 2x2 numpy arrays.
+        rhoSA_list : List[np.ndarray]
+            A list of the states rhoSA_i as numpy matrices (4x4), used
+            to compute the ancilla reduced state rhoA = sum_i Tr_S[rhoSA_i].
+        verbosity : int, optional
     Returns:
     -------
         p_guess : float
             The optimal guessing probability.
-        PiSA_list : list of np.ndarray
-            The list of optimal measurement operators PiSA_i.
+        PiSA_list : List[np.ndarray]
+            The list of optimal POVM elements PiSA_i (4x4) returned by PICOS.
     """
     dim = 4 # Dimension of the total system S+A
     problem = pic.Problem()
@@ -213,16 +238,14 @@ def find_meas_max_p_guess(M_S, rhoSA_list, verbosity=1):
 
     # Impose as a constraint TrA[PiSA_i * (1⊗rhoA)] = MS_i
     I2 = pic.Constant(np.eye(2))
-    for i, MS_i in enumerate(M_S):  
 
-        # rhoA = TrS[rhoSA]
-        rhoSA_pic = [pic.Constant(r) for r in rhoSA_list]
-        rhoA = [pic.partial_trace(r, 0) for r in rhoSA_pic]
-        rhoA = pic.sum(rhoA)
-        
-        # TrA[PiSA_i * (1⊗rhoA)]
+    rhoSA_pic = [pic.Constant(r) for r in rhoSA_list]
+    rhoA = [pic.partial_trace(r, 0) for r in rhoSA_pic]
+    rhoA = pic.sum(rhoA)
+
+    for i, MS_i in enumerate(M_S):  
         expr = pic.partial_trace(PiSA_list[i] * (I2 @ rhoA), 1)
-        problem.add_constraint(expr == MS_i.full())
+        problem.add_constraint(expr == MS_i)
 
 
     # -------------------- Objective -------------------- #
@@ -252,21 +275,17 @@ if __name__ == "__main__":
     rhoSA, probabilities = find_entangled_rhoSA(basis)
     assert np.isclose(sum(probabilities), 1), "!!! Probabilities do not sum to 1."
 
-    # Or use uniform probabilities ( like in the paper ).
-    # probabilities = [1/4]*4
-    # rhoSA = sum([p * (psi * psi.dag()) for psi, p in zip(basis, probabilities)])
-
     rhoSA_list = [p * (psi * psi.dag()).full() for psi, p in zip(basis, probabilities)]
     assert np.isclose(sum([np.trace(rhoSA_i) for rhoSA_i in rhoSA_list]), 1), "!!! rhoSA doesn't have trace 1."
 
     # Check entanglement of rhoSA.
     check_SA_entanglement(rhoSA)
 
-    # rhoS = Tr_A(rhoSA).
+    # rhoS = Tr_A(rhoSA) and rhoA = Tr_S(rhoSA).
     rhoS = rhoSA.ptrace(0).full()
     rhoA = rhoSA.ptrace(1).full()
 
-    
+
     # ----------------- Measurement ----------------- #
     # PiSA_i = |Ψ_i⟩⟨Ψ_i|.
     PiSA = [psi * psi.dag() for psi in basis]
@@ -276,6 +295,7 @@ if __name__ == "__main__":
     I2 = qt.qeye(2)
     x = qt.tensor(I2, qt.Qobj(rhoA))
     M_S = [(PiSA_i @ x).ptrace(0) for PiSA_i in PiSA]
+      ## M_S = [((PiSA_i * x).ptrace(0)).full() for PiSA_i in PiSA]
 
 
     # ------------- Seesaw optimization ------------- #
@@ -286,11 +306,11 @@ if __name__ == "__main__":
     for _ in tqdm(range(attempts)):
         
         old_p_guess, PiSA_list = find_meas_max_p_guess(M_S, rhoSA_list, verbosity=1) 
-
+        PiSA_list = [np.array(P) for P in PiSA_list]  # convert to numpy again
         diffs.append(new_p_guess - old_p_guess) # Collect data for the plot.
         
         new_p_guess, rhoSA_list = find_sep_states_max_p_guess(rhoS, PiSA_list, verbosity=1)
-
+        rhoSA_list = [np.array(r) for r in rhoSA_list]  # convert to numpy again
         diffs.append(new_p_guess - old_p_guess) # Collect data for the plot.
 
         if abs(new_p_guess - old_p_guess) < precision:
@@ -306,3 +326,4 @@ if __name__ == "__main__":
     plt.xlabel('Iteration')
     plt.ylabel('Difference in Guessing Probability')
     plt.show()  
+
